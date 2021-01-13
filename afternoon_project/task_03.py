9a34f2e06a33b1b1f39411823e3392208d999191
Olivia Toolson <livytoolson@gmail.com>
2021-01-13 16:13:57 -0700

    added other solutions to task 3

--- afternoon_project/task_03.py
+++ afternoon_project/task_03.py
@@ -13,6 +13,12 @@ input_str will only contain lowercase alpha characters.
 [output] integer
 """
 
+# find character with a frequency of 1
+# traverse through input string 
+# use .count() method
+# if count is > 1, return -1
+# return index of first character with a count of 1
+
 def csFirstUniqueChar(input_str):
     for s in input_str:
         if input_str.count(s) == 1:
@@ -23,8 +29,28 @@ print(csFirstUniqueChar(input_str = "lambdaschool"))
 print(csFirstUniqueChar(input_str = "ilovelambdaschool"))
 print(csFirstUniqueChar(input_str = "vvv"))
 
-# find character with a frequency of 1
-# traverse through input string 
-# use .count() method
-# if count is > 1, return -1
-# return index of first character with a count of 1
+# def csFirstUniqueChar(input_str):
+#     for letter in input_str:
+#         li = input_str.find(letter) + 1
+#         if letter not in input_str[li:]:
+#             return input_str.find(letter)
+#     return -1
+
+import nump as nump
+def csFirstUniqueChar(input_str):
+    y = list(input_str)
+    x = np.unique(y, return_index=True, return_counts=True)
+    z = []
+    for i, j enumerate(x[2]):
+        if x[1][i] < z and j == 1:
+            z = x[1][i]
+        print (i, j, x[1][i])
+    if z == 9999:
+        z = -1
+    return z
+
+# could use a dictionary to keep track of the total of all the letters 
+# take a second pass through the string  
+
+# return -1 if z == 9999 else z
+# return z == 9999 ? -1:z --> the Ternary Operator
